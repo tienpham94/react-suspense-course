c42ef83cdfb6562bbad4783aad2320eee746c1a8
Tien Pham <tiendev94@gmail.com>
2020-04-30 21:43:30 +0300

    err boundary

--- src/lessons/102/app.js
+++ src/lessons/102/app.js
@@ -1,6 +1,32 @@
 import React from "react";
 // 1. Create an import error by giving React.lazy a Promise.reject()
-const PokemonDetail = React.lazy(() => import("./pokemon-detail"));
+const PokemonDetail = React.lazy(() => Promise.reject());
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback
+    }
+
+    return this.props.children;
+  }
+}
 
 // 2. Check the console for errors about error boundaries
 
@@ -11,9 +37,11 @@ export default function App() {
   return (
     <div>
       {/* 6. Wrap your Suspense Component in the freshly minted ErrorBoundary component */}
-      <React.Suspense fallback={"Loading your Pokemon..."}>
-        <PokemonDetail />
-      </React.Suspense>
+      <ErrorBoundary fallback="Couldn't catch em all">
+        <React.Suspense fallback={"Loading your Pokemon..."}>
+          <PokemonDetail />
+        </React.Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
