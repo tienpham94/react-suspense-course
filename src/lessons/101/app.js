618821cd5d129465d8c5e683e56a5ee346b36eb3
Tien Pham <tiendev94@gmail.com>
2020-04-30 21:37:49 +0300

    suspense

--- src/lessons/101/app.js
+++ src/lessons/101/app.js
@@ -1,12 +1,15 @@
 import React from "react";
 // 1. Change this static import to a dynamic import, wrapped in React.lazy
-import PokemonDetail from "./pokemon-detail";
+const PokemonDetail = React.lazy(() =>import('./pokemon-detail'))
 
 export default function App() {
   return (
     <div>
       {/* 2. Wrap this component in a React.Suspense component with fallback */}
+      <React.Suspense fallback="loading pokemon">
+
       <PokemonDetail />
+      </React.Suspense>
     </div>
   );
 }
