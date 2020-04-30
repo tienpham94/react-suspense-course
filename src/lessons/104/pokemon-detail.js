20810b3a401b30b0db9302f698de3d5321c05c82
Tien Pham <tiendev94@gmail.com>
2020-04-30 21:57:21 +0300

    suspense wrapper

--- src/lessons/104/pokemon-detail.js
+++ src/lessons/104/pokemon-detail.js
@@ -1,40 +1,45 @@
 import React from "react";
 
 // 1. Uncomment this baseline `suspensify` function for communicating promise status to React
-// function suspensify(promise) {
-//   let status = "pending";
-//   let result;
-//   let suspender = promise.then(
-//     response => {
-//       status = "success";
-//       result = response;
-//     },
-//     error => {
-//       status = "error";
-//       result = error;
-//     }
-//   );
+function suspensify(promise) {
+  let status = "pending";
+  let result;
+  let suspender = promise.then(
+    response => {
+      status = "success";
+      result = response;
+    },
+    error => {
+      status = "error";
+      result = error;
+    }
+  );
 
-//   return {
-//     read() {
-//       if (status === "pending") {
-//       }
-//       if (status === "error") {
-//         throw result;
-//       }
-//       if (status === "success") {
-//         return result;
-//       }
-//     }
-//   };
-// }
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      }
+      if (status === "error") {
+        throw result;
+      }
+      if (status === "success") {
+        return result;
+      }
+    }
+  };
+}
 
 // 2. Fetch a pokemon from PokeAPI and store `pokemon` variable
 //    fetch(`https://pokeapi.co/api/v2/pokemon/1`)
 // 3. Wrap this fetch request in the `suspensify` function
 
+let pokemon = suspensify(
+  fetch("https://pokeapi.co/api/v2/pokemon/1").then(res => res.json())
+);
+
 export default function PokemonDetail() {
   // 4. `pokemon` is now a resource with a `read()` function on it
   //    use `{pokemon.read().name}` to display the name of the first Pokemon fetched from the internet
-  return <div>Static Pokemon</div>;
+  return <div>{pokemon.read().name}</div>;
 }
